8ffa56593323472f84614a201968dbce6f3219e3
countryqueen <31453544+countryqueen@users.noreply.github.com>
2017-10-13 13:52:45 -0500

    Add files via upload

---
+++ AngelicaDS4/src/angelicads4/BTreeNode.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package angelicads4;
+
+/**
+ *
+ * @author Owner
+ */
+public class BTreeNode 
+{
+    //these are public variables
+    public String value;
+    public int counter;
+    public BTreeNode left;
+    public BTreeNode right;
+    
+    //the counter is used to count how many words are in the tree. 
+    //the string is used for the words in the document.
+}
